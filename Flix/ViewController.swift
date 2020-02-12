8e7d1bd572f337da662467e197d6d22b6d9ae73f
tnyprk <tonysj.park97@gmail.com>
2020-02-12 12:31:12 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Tony Park on 2/12/20.
+//  Copyright © 2020 Tony Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
